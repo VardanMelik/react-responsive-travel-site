02aa59a3063155a92d0b6ff2aeb77bc49b3553a5
EYE MOTION <eyemotiongamestudio@gmail.com>
2020-12-18 23:59:33 +0400

    update

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,11 +1,24 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom'; 
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <=960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+    window.addEventListener('resize', showButton);
+
     return (
         <div>
             <nav className="navbar">
@@ -22,7 +35,23 @@ function Navbar() {
                                 Home
                             </Link>
                         </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Sign-Up
+                            </Link>
+                        </li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
                 </div>
             </nav>
         </div>
