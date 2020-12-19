323968c8725c494ca130b015bb7c3ce035a2f312
EYE MOTION <eyemotiongamestudio@gmail.com>
2020-12-19 16:08:11 +0400

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './component/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 
 'react-router-dom';
+import Home from './component/pages/Home';
 
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" exact />
+        <Route 
+          path="/" 
+          exact 
+          component={Home} />
       </Switch>
     </Router>
   );
